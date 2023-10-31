f2cfb8d56f64a60117bb3b70284de880b6b0438b
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-10-31 14:56:01 -0500

    Create superduperit.py

---
+++ superduperit.py
@@ -0,0 +1,17 @@
+import os
+
+# Uncomment one of the following lines to use a bespoke MongoDB deployment
+# For testing the default connection is to mongomock
+
+mongodb_uri = os.getenv("MONGODB_URI","mongomock://test")
+# mongodb_uri = "mongodb://localhost:27017"
+# mongodb_uri = "mongodb://superduper:superduper@mongodb:27017/documents"
+# mongodb_uri = "mongodb://<user>:<pass>@<mongo_cluster>/<database>"
+# mongodb_uri = "mongodb+srv://<username>:<password>@<atlas_cluster>/<database>"
+
+CFG.downloads.hybrid = True
+CFG.downloads.root = './'
+
+# Super-Duper your Database!
+from superduperdb import superduper
+db = superduper(mongodb_uri)
